f05db2376dc4ad4077e1bdc0e4cda44f85b9fde0
Carolina Moura <carolinamouravalle@yahoo.com>
2025-01-14 16:11:39 -0600

    Support new encoding in the interface

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import streamlit as st
 from PIL import Image
 import io
+from typing import NewType
 
-AVAILABLE_DIMENSIONS = [(320, 320)]
+####### UTILS #######
+
+Int16 = NewType('Int16', int)
+Int8 = NewType('Int8', int)
+
+def get_high_and_low_bytes(num: Int16) -> list[Int8, Int8]:
+    """
+    Given a 16-bit integer, returns a tuple containing the high and low bytes.
+    """
+    high_byte = (num >> 8) & 0xFF
+    low_byte = num & 0xFF
+    return [high_byte, low_byte]
+
+####### IMAGE FUNCTIONS #######
 
 def convert_image_to_rgb565(image, width, height):
     img = image.resize((width, height), Image.Resampling.BILINEAR)
@@ -26,23 +40,79 @@ def process_image(image, dimensions: tuple[int, int], caption: str):
 
     byte_array = bytearray()
 
-    # Add the dimensions of the image as the first four bytes
+    # Dimensions
     byte_array.extend([ width >> 8, width & 0xFF, height >> 8, height & 0xFF])
 
-    # Next byte is the size of the caption (maximum of 256 characters, so 1 byte)
-    byte_array.extend([len(caption)])
+    # Size of the caption (maximum of 256 characters, so 1 byte)
+    # byte_array.extend([len(caption.encode())])
+    byte_array.extend(caption.encode() + b'\0')
 
-    # Add the caption as bytes
-    byte_array.extend(caption.encode())
+    data_1 = encoding_1(rgb565_data)
+    data_2 = encoding_2(rgb565_data)
 
-    for color in rgb565_data:
-        high_byte = (color >> 8) & 0xFF
-        low_byte = color & 0xFF
-        byte_array.extend([high_byte, low_byte])
+    print(data_1[:30])
+    print(f"Encoding 2 is {len(data_2)/len(data_1)*100}% of the original encoding")
+    print(data_2[:30])
+    
+    typ = 0
+    data = data_1
 
+    if len(data_1) > len(data_2):
+        typ = 1
+        data = data_2
+
+    byte_array.extend([typ])
+    byte_array.extend(data)
+    
     return byte_array
 
+def encoding_1(rgb565_data: list[Int16]) -> list[Int8]:
+    """
+    Encodes a list of 16-bit RGB565 color values into a list of 8-bit values.
+    """
+    final_data: list[Int8] = []
+    for color in rgb565_data:
+        final_data.extend(get_high_and_low_bytes(color))
+    return final_data
+
+def encoding_2(rgb565_data: list[Int16]) -> list[Int8]:
+    """
+    Encodes a list of RGB565 data using a more efficient method.
+    Args:
+        rgb565_data (list[Int16]): A list of 16-bit integers representing RGB565 encoded data.
+    Returns:
+        list[Int8]: A list of 8-bit integers representing the encoded RGB565 data according
+                    to encoding algorithm 2.
+    """
+    def count_contiguous(data: list[Int16], start: int) -> int:
+        """
+        Counts the number of contiguous elements in the list starting from the given index.
+        """
+        count = 1
+        for i in range(start + 1, len(data)):
+            if data[i] == data[start]:
+                count += 1
+            else:
+                break      
+        return count
+
+    final_data: list[Int8] = []
+
+    ptr = 0
+    while ptr < len(rgb565_data):
+        count = count_contiguous(rgb565_data, ptr)
+        pixel = get_high_and_low_bytes(rgb565_data[ptr])
+        while count > 0:
+            qtt = min(0xFF, count)
+            final_data.extend([qtt, *pixel])
+            count -= qtt 
+            ptr += qtt
+    
+    return final_data
+
 ###### Streamlit Interface ######
+AVAILABLE_DIMENSIONS = [(320, 320)]
+
 def main():
     st.set_page_config(page_title="Assistive Voices", page_icon=":smiley:")
     st.title("Image Processor for Assistive Voices :smiley: :microphone:")
