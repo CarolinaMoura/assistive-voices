eb28c9ebfeb5b23b805fe913cb015d53e4f050da
Carolina Moura <carolinamouravalle@yahoo.com>
2025-01-23 19:03:10 -0600

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-from io import BytesIO
-import streamlit as st
-sound_file = BytesIO()
-tts = gTTS('Alacrán', lang='es', tld="com.mx")
-tts.write_to_fp(sound_file)
-st.audio(sound_file)
